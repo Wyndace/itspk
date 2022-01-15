d131ad69d5df287fc3780f98732426cd61206fe3
Alexander (Wyndace) <wyndace.av@ya.ru>
2022-01-16 02:25:09 +0500

    added scrollDisabling.js

--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -1,15 +1,11 @@
 const burgers = document.querySelectorAll("[data-burger]");
-console.log(burgers);
 if (burgers.length > 0) {
   burgers.forEach((burger) => {
-    const burgersButton = burger.querySelectorAll("[data-burger_button]");
-    console.log(burgersButton);
-    burgersButton.forEach((burgerButton) => {
-      burgerButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log(burger);
+    burger.addEventListener("click", (e) => {
+      if (e.target.closest("[data-burger_button]") || !e.target.closest("[data-burger_body]") || (e.target.closest("a") && !e.target.closest("a").href.split("/")[e.target.closest("a").href.split("/").length - 1].includes(":"))) {
         burger.classList.toggle("_active");
-      });
+        toggleScroll(document.body, true);
+      }
     });
   });
 }
