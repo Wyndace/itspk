7ca3ab5c5bc82e1d1fe73547bcd43681b976742d
Васильев Александр <wyndace.av@ya.ru>
2022-06-04 00:09:04 +0500

    minor fixes

--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -2,7 +2,7 @@ const burgers = document.querySelectorAll("[data-burger]");
 if (burgers.length > 0) {
   burgers.forEach((burger) => {
     burger.addEventListener("click", (e) => {
-      if (e.target.closest("[data-burger_button]") || !e.target.closest("[data-burger_body]") || (e.target.closest("a") && !e.target.closest("a").href.split("/")[e.target.closest("a").href.split("/").length - 1].includes(":"))) {
+      if (e.target.closest("[data-burger_button]") || !e.target.closest("[data-burger_body]") || (e.target.closest("a") && !e.target.closest("a").href.split("/")[e.target.closest("a").href.split("/").length - 1].includes(":")) || e.target.closest('[data-modal]') || e.target.closest('[data-scroll_to]')) {
         burger.classList.toggle("_active");
         toggleScroll(document.body, true);
       }
