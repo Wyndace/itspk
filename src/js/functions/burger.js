d5c0abcdacd696ea17f87999242451e96c8a555d
Васильев Александр <wyndace.av@ya.ru>
2022-06-11 00:26:05 +0500

    minor fixes

--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -4,7 +4,7 @@ if (burgers.length > 0) {
     burger.addEventListener("click", (e) => {
       if (e.target.closest("[data-burger_button]") || !e.target.closest("[data-burger_body]") || (e.target.closest("a") && !e.target.closest("a").href.split("/")[e.target.closest("a").href.split("/").length - 1].includes(":")) || e.target.closest('[data-modal]') || e.target.closest('[data-scroll_to]')) {
         burger.classList.toggle("_active");
-        toggleScroll(document.body, true);
+        if (!e.target.closest('[data-modal]')) toggleScroll(document.body, true);
       }
     });
   });
