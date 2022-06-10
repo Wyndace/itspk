5f09e2f88ef9eac91db90987e9dec8fa9db593d3
Васильев Александр <wyndace.av@ya.ru>
2022-06-11 00:21:05 +0500

    fixed clicking animation on buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 // === JS for index.html =========================================================================================================================================
-
-fileInclude.include("./components/ex.js");
+fileInclude.include("./components/buttonTappingAnimation.js");
+document.addEventListener('click', (e) => {
+    if (e.target.closest("button")) {
+        buttonTapingAnimation(e.target.closest("button"), 300);
+    }
+});
 
 // ====================================================================================================================================================================
